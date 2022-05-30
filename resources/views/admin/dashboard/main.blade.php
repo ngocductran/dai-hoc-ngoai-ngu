f7c9eb7759ea23397e3a66c53a9f6cda4a9233ba
NDT <ngocductran2000@gmail.com>
2022-05-30 20:36:31 +0700

    push code

---
+++ resources/views/admin/dashboard/main.blade.php
@@ -0,0 +1,19 @@
+<div class="main-panel">
+    <div class="content">
+        <div class="page-inner">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <div class="card-title">Card Title</div>
+                            <div>Thêm</div>
+                        </div>
+                        <div class="card-body">
+                            Main
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Atlantis Lite - Bootstrap 4 Admin Dashboard</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+	<link rel="icon" href="{{asset('assets/img/icon.ico')}}" type="image/x-icon"/>
+
+	<!-- Fonts and icons -->
+	<script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+
+	<!-- CSS Files -->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/atlantis.min.css')}}">
+
+</head>
+<body>
+	<div class="wrapper">
+		
+        <!-- Navbar Header -->
+        @include('admin.dashboard.navbar')
+        <!-- End Navbar -->
+
+		<!-- Sidebar -->
+        @include('admin.dashboard.sidebar')
+		<!-- End Sidebar -->
+
+        <!-- Main -->
+        @include('admin.dashboard.main')
+		<!-- End Main -->
+		
+	</div>
+	<!--   Core JS Files   -->
+	<script src="{{asset('assets/js/core/jquery.3.2.1.min.js')}}"></script>
+	<script src="{{asset('assets/js/core/popper.min.js')}}"></script>
+	<script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+
+	<!-- jQuery UI -->
+	<script src="{{asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
+	<script src="{{asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
+
+	<!-- jQuery Scrollbar -->
+	<script src="{{asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
+
+	<!-- Datatables -->
+	<script src="{{asset('assets/js/plugin/datatables/datatables.min.js')}}"></script>
+
+	<!-- jQuery Vector Maps -->
+	<script src="{{asset('assets/js/plugin/jqvmap/jquery.vmap.min.js')}}"></script>
+	<script src="../assets/js/plugin/jqvmap/maps/jquery.vmap.world.js')}}"></script>
+
+	<!-- Sweet Alert -->
+	<script src="{{asset('assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
+
+	<!-- Atlantis JS -->
+	<script src="{{asset('assets/js/atlantis.min.js')}}"></script>
+</body>
+</html>
