45223aaffc99712d81f2aaa355d3382ed671ac01
NDT <ngocductran2000@gmail.com>
2022-06-10 23:09:21 +0700

    update code

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -8,7 +8,7 @@
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{url('assets/home/css/style.css')}}">
 
 </head>
 <body>
